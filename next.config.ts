191ef60be21523228e888c1c58e704022811854c
ojan <muhammadfauzan292004@gmail.com>
2026-06-12 18:35:09 +0700

    chore: reset config for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? "/KreaNation" : "",
+  /* config options here */
 };
 
 export default nextConfig;
